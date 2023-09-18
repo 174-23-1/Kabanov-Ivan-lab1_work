2504ac8232dd9f5d7c3813333ba2948a209b27de
User <ivanko252006@gmail.com>
2023-09-18 12:11:39 +0300

    project

---
+++ project/project.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+
+int main()
+{
+    const int x{ 5 }, z{ 13 };
+    const float y{ 3.3 };
+
+    const double result{ (13*z-y*y*z+1.1*z/2) / (1.47*y+18*z) * (0.03)};
+    std::cout << "Result = " << result;
+}
